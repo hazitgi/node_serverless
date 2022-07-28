5f87bb5eed321b62053a03f017b1b41e0c5fe163
hazitgi <hazitgi@gmail.com>
2022-07-28 12:55:02 +0530

    insert item into DynamoDB

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,27 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
+  await DynamoDB.put({
+    TableName: "AuctionTable",
+    Item: auction,
+  }).promise()
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify({ auction }),
   };
 }
 
 export const handler = createAuction;
-
-
