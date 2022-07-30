e21d5d0dbd38e1136e6e7bf9f4c840d1f27887be
Haseeb K <mohamedhaseeb@ateamsoftsolutions.com>
2022-07-30 14:33:42 +0530

    error handling

--- src/handlers/getSingleAuction.js
+++ src/handlers/getSingleAuction.js
@@ -4,18 +4,32 @@ import createError from "http-errors";
 
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
-async function getSingleAuction(event, context) {
+export const getAuctionById = async (id) => {
   let auction;
-  const { id } = event.pathParameters;
   try {
     const result = await DynamoDB.get({
       TableName: process.env.AUCTION_TABLE_NAME,
       Key: { id },
     }).promise();
+
     auction = result.Item;
-    if (!auction) {
-      throw new createError.NotFound(`Auction with ID "${id}" not found!`);
-    }
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ID "${id}" not found!`);
+  }
+  return auction
+};
+
+
+async function getSingleAuction(event, context) {
+
+  const { id } = event.pathParameters;
+  try {
+   const auction = await getAuctionById(id)
+
     return {
       statusCode: 200,
       body: JSON.stringify(auction),
--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -1,12 +1,21 @@
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import { getAuctionById } from "./getSingleAuction";
 
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+
+  const auction = await getAuctionById(id);
+
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid must be higher than ${auction.highestBid.amount}`
+    );
+  }
 
   const params = {
     TableName: process.env.AUCTION_TABLE_NAME,
