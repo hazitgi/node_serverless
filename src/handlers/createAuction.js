33f65d119cb2956a590fb75e480f5e015ffd7c3f
Haseeb K <mohamedhaseeb@ateamsoftsolutions.com>
2022-07-30 20:03:32 +0530

    validation applied on create auctno

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,6 +2,8 @@ import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import validator from "@middy/validator";
+import createAuctionSchema from "../lib/schemas/createAuctionSchema";
 
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
@@ -39,4 +41,6 @@ async function createAuction(event, context) {
     body: JSON.stringify({ auction }),
   };
 }
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(
+  validator({ inputSchema: createAuctionSchema })
+);
